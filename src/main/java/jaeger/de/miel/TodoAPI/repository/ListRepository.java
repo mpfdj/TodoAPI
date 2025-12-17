648545a05135d140d1fa3e6562d059045fa5295a
Miel <miel.de.jaeger@ing.com>
2025-12-17 16:01:57 +0100

    Changed CrudRepository to Jpa

--- src/main/java/jaeger/de/miel/TodoAPI/repository/ListRepository.java
+++ src/main/java/jaeger/de/miel/TodoAPI/repository/ListRepository.java
@@ -1,15 +1,15 @@
 package jaeger.de.miel.TodoAPI.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ListRepository extends CrudRepository<jaeger.de.miel.TodoAPI.entity.List,Long> {
+public interface ListRepository extends JpaRepository<jaeger.de.miel.TodoAPI.entity.List,Long> {
 
 //    @Query("SELECT l FROM List l WHERE l.owner.id = :ownerId")
 //    List<jaeger.de.miel.TodoAPI.entity.List> findListsByUserId(@Param("ownerId") Long ownerId);
 
-    List<jaeger.de.miel.TodoAPI.entity.List> findListsByOwner_Id(Long ownerId);
+    List<jaeger.de.miel.TodoAPI.entity.List> findListsByOwner_Id(Long ownerId);  // Using JPA derived query method name
     boolean existsByOwner_IdAndNameIgnoreCase(Long ownerId, String name);
 
 }
--- src/main/java/jaeger/de/miel/TodoAPI/repository/UserRepository.java
+++ src/main/java/jaeger/de/miel/TodoAPI/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package jaeger.de.miel.TodoAPI.repository;
 
 import jaeger.de.miel.TodoAPI.entity.AppUser;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<AppUser, Long> {
+public interface UserRepository extends JpaRepository<AppUser, Long> {
 
     boolean existsByEmail(String email);
     void deleteById(Long id);
-    void deleteByEmail(String email);
+    void deleteByEmail(String email);  // Using JPA derived query method name
+
 
 //    @Transactional
 //    @Modifying
