822029afdc826bda8fa8512939f22c1cbb41aa17
Miel <mpf.dejaeger@gmail.com>
2025-12-30 13:38:14 +0100

    Parameterized scripts

--- h2-server.cmd
+++ h2-server.cmd
@@ -1,9 +1,15 @@
 @echo on
 
-java -cp C:\Users\TO11RC\.m2\repository\com\h2database\h2\2.4.240\h2-2.4.240.jar org.h2.tools.Server ^
+set homeDir=C:\Users\mpfde
+set baseDir=C:\Users\mpfde\OneDrive\Documents\github\TodoAPI\h2
+
+::set homeDir=C:\Users\TO11RC
+::set baseDir=C:\Users\TO11RC\OneDrive - ING\miel\workspace\Java\TodoAPI\h2
+
+java -cp "%homeDir%\.m2\repository\com\h2database\h2\2.4.240\h2-2.4.240.jar" org.h2.tools.Server ^
     -tcp ^
     -web ^
     -tcpAllowOthers ^
     -tcpPort 9092 ^
     -webPort 9093 ^
-    -baseDir "C:\Users\TO11RC\OneDrive - ING\miel\workspace\Java\TodoAPI\h2"
+    -baseDir "%baseDir%"
